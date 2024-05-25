ca953f872c9835f1abad09e916b4c0362615d172
h.naeima <hassannaeima@gmail.com>
2024-05-25 13:17:30 +0330

    Dummy

--- Dummy.java
+++ Dummy.java
@@ -6,6 +6,8 @@ public class Dummy {
     short height_cm= 169;
     int weight_gr=94000;
     long mobileNumber= 9357466831L ;
-    float shoeSize=43.5f ;
+    double shoeSize=43.5 ;
+    float workInDotin_y= 3.5f ;
+    boolean isMen=true ;
 
 }
