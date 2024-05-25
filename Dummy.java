9e50ff543a1b6b16c3d5163b5ac1bfca86155eb2
h.naeima <hassannaeima@gmail.com>
2024-05-25 13:03:39 +0330

    Dummy

---
+++ Dummy.java
@@ -0,0 +1,11 @@
+public class Dummy {
+    char firstWordName ='n' ;
+    String name="niaz";
+    String family="naeima";
+    byte age=35 ;
+    short height_cm= 169;
+    int weight_gr=94000;
+    long mobileNumber= 9357466831L ;
+    float shoeSize=43.5f ;
+
+}
