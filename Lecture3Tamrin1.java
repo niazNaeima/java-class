350081b0e90872ac3c2b3c705b662b8bac097857
niaz <hassannaeima@gmail.com>
2024-06-01 01:23:06 +0430

    Lecture3Tamrin1

--- Lecture3Tamrin1.java
+++ Lecture3Tamrin1.java
@@ -3,7 +3,7 @@ public class Lecture3Tamrin1 {
         String str3=String.format("My name is %s and my age is %s","Niaz",35);
         String str4= str3.toUpperCase();
         String str5=str4.replace("NIAZ","Fanap");
-        String str6=str5.substring(0,str5.indexOf('F') +"Fanap".length());
+        String str6=str5.substring(0,str5.indexOf("Fanap") +"Fanap".length());
         String str7=str6.toLowerCase();
         System.out.println(str7);
 
