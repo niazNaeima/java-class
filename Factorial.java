56ec270298deb65a77f5090ef9e2ef95d8076761
h.naeima <hassannaeima@gmail.com>
2024-06-12 13:23:05 +0330

    Tamrin2

---
+++ Factorial.java
@@ -0,0 +1,23 @@
+import com.sun.deploy.security.SelectableSecurityManager;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner reader=new Scanner(System.in);
+        System.out.println("Please Enter Number");
+
+        int num=reader.nextInt();
+        System.out.println("Factorial Your Number is " +factorial(num));
+    }
+
+    public static int factorial(int n) {
+        if (n != 0) {
+            return n * factorial(n - 1);
+            }
+            else{
+                return 1;
+            }
+            }
+
+    }
